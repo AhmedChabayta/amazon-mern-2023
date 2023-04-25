b7acbd5ac6b73f8f44f7c28c847955abcd22f750
AhmedChabayta <chabays@gmail.com>
2023-04-25 13:47:38 +0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <header>TS Amazon</header>
+      <main></main>
+      <footer>All Rights Reserved</footer>
+    </div>
+  );
+}
+
+export default App;
